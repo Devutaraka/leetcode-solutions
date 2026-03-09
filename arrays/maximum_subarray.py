1dd95ec3189a307ff09e05d2b1148623a56468aa
Devutaraka <tarakadevu1@gmail.com>
2026-03-09 19:24:59 +0530

    Add Maximum Subarray solution

--- arrays/maximum_subarray.py
+++ arrays/maximum_subarray.py
@@ -1,1 +1,28 @@
+# LeetCode Problem: Maximum Subarray
+# https://leetcode.com/problems/maximum-subarray/
 
+"""
+Problem:
+Find the contiguous subarray with the largest sum.
+
+Approach:
+Kadane's Algorithm.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+def maxSubArray(nums):
+    current_sum = nums[0]
+    max_sum = nums[0]
+
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
+# Example
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+print(maxSubArray(nums))
