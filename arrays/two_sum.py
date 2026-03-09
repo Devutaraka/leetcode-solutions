84c0614800c3d561b6bc97317558b261fb3ac24c
Devutaraka <tarakadevu1@gmail.com>
2026-03-09 19:18:26 +0530

    Improve Two Sum solution with explanation

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,1 +1,34 @@
+# LeetCode Problem: Two Sum
+# https://leetcode.com/problems/two-sum/
 
+"""
+Problem:
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+
+Approach:
+Use a hashmap to store numbers and their indices.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
+
+def twoSum(nums, target):
+    hashmap = {}
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+
+        if complement in hashmap:
+            return [hashmap[complement], i]
+
+        hashmap[nums[i]] = i
+
+    return []
+
+
+# Example test
+nums = [2, 7, 11, 15]
+target = 9
+
+print(twoSum(nums, target))
