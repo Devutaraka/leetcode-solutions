65d0ed604cb0bb3363f85e14e96472075f9e6e03
Devutaraka <tarakadevu1@gmail.com>
2026-03-09 19:24:34 +0530

    Add Best Time to Buy and Sell Stock solution

--- arrays/best_time_to_buy_stock.py
+++ arrays/best_time_to_buy_stock.py
@@ -1,1 +1,31 @@
+# LeetCode Problem: Best Time to Buy and Sell Stock
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+"""
+Problem:
+Find the maximum profit from buying and selling a stock.
+
+Approach:
+Track the minimum price and compute max profit.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
+
+    return max_profit
+
+
+# Example
+prices = [7,1,5,3,6,4]
+print(maxProfit(prices))
